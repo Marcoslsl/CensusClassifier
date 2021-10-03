c7f7a0a6dfbe9311d4e4dafb1f25d57d3bb7f0e4
Marcos Vinicius <84594190+Marcoslsl@users.noreply.github.com>
2021-10-03 15:26:32 -0300

    Add files via upload

---
+++ api/handler.py
@@ -0,0 +1,37 @@
+import pickle
+import pandas as pd
+from flask         import Flask, request, Response
+from census.Census import Census
+
+model = pickle.load( open( 'C:/Users/Marcos/Projects/MachineLearningAZ/model/random_forest_tuned.pkl', 'rb' ) )
+
+app = Flask( __name__ )
+@app.route( '/census/predict', methods=['POST'] )
+
+def census_predict():
+    test_json = request.get_json()
+    
+    if test_json:
+        if isinstance( test_json, dict ):
+            test_raw = pd.DataFrame( test_json, index=[0] )
+        else:
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+        
+        # Instantiate Census Class
+        pipeline = Census()
+        
+        # data selection
+        df1 = pipeline.data_selection( test_raw )
+        
+        # data preparation
+        df2 = pipeline.data_preparation( df1 )
+        
+        # data prediction
+        df_response = pipeline.get_predictions( model, test_raw, df2 )
+        
+        return df_response
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+    
+if __name__ == '__main__':
+    app.run( '0.0.0.0' )
